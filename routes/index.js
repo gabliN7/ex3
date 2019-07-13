6b9c53f377a332706f9c65dc024b74608579f951
Thanayut Suwannasin <59143249@g.cmru.ac.th>
2019-07-13 15:36:31 +0700

    เพิ่มสักอย่าง

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'test' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'test' });
+});
+
+module.exports = router;
